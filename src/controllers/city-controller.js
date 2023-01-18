fd680dd7882ed05a22f01ba248908e7fcacb8f3d
drkspark <madhusudan753@gmail.com>
2023-01-18 23:23:11 +0530

    Added get all cities API

--- src/controllers/city-controller.js
+++ src/controllers/city-controller.js
@@ -90,10 +90,32 @@ const update = async (req, res) => {
     }   
 }
 
+const getAll = async (req, res)  => {
+    try {
+        const cities = await cityService.getAllCities();
+        return res.status(201).json({
+            data: cities,
+            success: true,
+            message: "Successfully fetched all the cities",
+            err: {}
+        });
+    }
+    catch (error){
+        console.log(error);
+        return res.status(500).json({
+            data: {},
+            success: false,
+            message: "Not able to fetch the cities",
+            err: error
+        });
+    }
+
+}
 
 module.exports = {
     create,
     destroy,
     get,
-    update
+    update,
+    getAll
 }
