af9fe140bc16bcd923db0bacde089af51f11e1ea
drkspark <madhusudan753@gmail.com>
2023-01-23 06:35:45 +0530

    Added flight create CRUD

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const CityController = require("../../controllers/city-controller");
+const FlightController = require("../../controllers/flight-controller");
 
 const router = express.Router();
 
-router.post('/city', CityController.create);
-router.delete('/city/:id', CityController.destroy);
+//! City Routes
+router.post("/city", CityController.create);
+router.delete("/city/:id", CityController.destroy);
 router.get("/city/:id", CityController.get);
 router.patch("/city/:id", CityController.update);
 router.get("/city", CityController.getAll);
 
+//! Flights Routes
+router.post("/flights", FlightController.create);
 
 module.exports = router;
